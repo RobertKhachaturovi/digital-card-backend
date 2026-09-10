34224feae1242ac9d9c073489dad332f23435378
RobertKhachaturovi <khachaturovirobert@gmail.com>
2026-09-10 11:38:59 +0400

    feat: implement GraphQL modules, resolvers, and services for skills, experiences, and projects

---
+++ src/experience/experience.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class ExperienceService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findByProfileId(profileId: string) {
+    return this.prisma.experience.findMany({
+      where: { profileId },
+    });
+  }
+}
